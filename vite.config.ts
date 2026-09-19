93fed02c9bcdf0808c75ae9c19dde61823d864fd
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2026-09-19 09:21:54 -0500

    boiler plate

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react(), basicSsl()],
+	server: {
+		https: true,
+	},
 })
